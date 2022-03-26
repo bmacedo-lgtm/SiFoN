f767161d9d031364b9f9fd446caa65bd116dc831
Briana E. Macedo <bmacedo@accio.Princeton.EDU>
2022-03-26 14:56:40 -0400

    fasta

--- src/SiFoN/construct_FASTA.py
+++ src/SiFoN/construct_FASTA.py
@@ -3,62 +3,60 @@ import pandas as pd
 import itertools
 import random
 
-def retrieve(ref_genome_fasta):
-    genome = Genome(input_path = ref_genome_fasta)
+alt_allele = {"A":"G", "G":"A", "T":"C", "C":"T"}
     
 def write_haps_of_pairs(data, ref_filename, alt_filename, shift_filename, centered_filename,
-         chrm, cutoff):
+         chrm, cutoff, gn):
     alt_file = open(alt_filename, "w")
     ref_file = open(ref_filename, "w")
     shift_file = open(shift_filename, "w") 
     centered_file = open(centered_filename, "w")
-    chrm, offset, cutoff = 2048, chrm, cutoff
+    offset = 2048
+    ID_to_alt_dict = dict(zip(data["ID"].values, data["ALT"].values))
     for pairs, ID in zip(itertools.combinations(data["POS"].values, 2), 
                             itertools.combinations(data["ID"].values, 2)):
-
+        
         if abs(pairs[0] - pairs[1] > cutoff): continue # skip if snps are too far apart
         if pairs[0] == pairs[1]: continue # skip if snps are at the same position
-        desc.append(write_to_file(pairs[0], pairs[1], 
+        write_to_file(pairs[0], pairs[1], chrm,
                      ref_file, alt_file, shift_file, centered_file, 
-                                 ID[0], ID[1]))
-        desc.append(write_to_file(pairs[1], pairs[0], 
+                                 ID[0], ID[1], ID_to_alt_dict, gn)
+        write_to_file(pairs[1], pairs[0], chrm,
                      ref_file, alt_file, shift_file, centered_file, 
-                                 ID[1], ID[0]))  
+                                 ID[1], ID[0], ID_to_alt_dict, gn)
     ref_file.close()
     alt_file.close()
     shift_file.close()
     centered_file.close()
-    
-    
-def writes_haps_around_SNP(data, ref_filename, alt_filename, shift_filename, centered_filename,
-         chrm, rng, center_SNP):
+     
+def writes_haps_around_SNP(ref_filename, alt_filename, shift_filename, centered_filename,
+         chrm, rng, center, gn):
     alt_file = open(alt_filename, "w")
     ref_file = open(ref_filename, "w")
     shift_file = open(shift_filename, "w") 
     centered_file = open(centered_filename, "w")
-    offset, chrm, rng, center_SNP = 2048, chrm, rng, center_SNP
+    offset = 2048
     for pairs in itertools.combinations(range(-rng, rng), 2):
-        write_to_file(center + pairs[0], center + pairs[1], 
-                     ref_file, alt_file, shift_file, centered_file)
-
-        write_to_file(center + pairs[1], center + pairs[0], 
+        write_to_file(center + pairs[0], center + pairs[1], chrm,
                      ref_file, alt_file, shift_file, centered_file, 
-                      center + pairs[1], center + pairs[0])  
+                     center + pairs[1], center + pairs[0], gn)
+
+        write_to_file(center + pairs[1], center + pairs[0], chrm,
+                     ref_file, alt_file, shift_file, centered_file, 
+                      center + pairs[1], center + pairs[0], gn)  
     ref_file.close()
     alt_file.close()
     shift_file.close()
     centered_file.close()
 
-
-def write_to_file(snp1, snp2, 
+def write_to_file(snp1, snp2, chrm,
                  ref_file, alt_file, shift_file, centered_file, 
-                 id1, id2):
-    seq = genome.get_sequence_from_coords(chrom = chrm,
+                 id1, id2, ID_to_alt_dict, gn):
+    offset = 2048
+    seq = gn.get_sequence_from_coords(chrom = chrm,
                                  start = snp1 - offset,
                                  end = snp1 + offset)
-    # add in reference haplotypes
     preface = str(str(snp1) +  "_" + str(snp2) + "_" + id1 + "_" + id2)
-    
     ref_file.write(">" + str(preface) + "\n" + seq + "\n")
     seq_list = list(seq)
     
